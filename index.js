4f386713f967da2b7abda9b7c3234556aefbd79a
Okem Onyeka <jesse007onyeka@gmail.com>
2023-11-24 16:03:09 +0100

    Added mongoose schema

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-// mongoose.connect('mongodb://localhost/subscribers');
-// const db = mongoose.connection;
-// db.o
-
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(port, () => console.log(`server running on port ${port}`));
+app.get('/book', (req, res) => {
+    res.send('Ore no na wa Okem-desu');
+});
+
+
+mongoose.
+connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log('connected to MongoDB');
+    app.listen(port, () => { console.log(`server running on port ${port}`) });
+}).catch((error) => {
+    console.log(error)
+});
+// const db = mongoose.connection;
+// db.o
+
+
+
